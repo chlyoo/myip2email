499495bb201964ab452bd20695b6022562bd94ab
= <changhyunlyoo@gmail.com>
2021-05-11 22:28:48 +0900

    first commit

---
+++ send_ip.py
@@ -0,0 +1,24 @@
+import socket
+import smtplib
+import socket
+from email.mime.text import MIMEText
+from config import * #중요정보는 config파일에 분리 저장
+
+def send_email(who, msg):
+	msg = MIMEText(msg)
+	msg['Subject'] = MAIL_SUBJECT
+	msg['To'] = who
+	s.sendmail(MAIL_SENDER, who, msg.as_string())
+
+if __name__ == '__main__':
+	#소켓을 이용해 아이피 확인
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(("8.8.8.8", 80))
+	ip = s.getsockname()[0]
+	print(s.getsockname()[0])
+	s.close()
+	#smtp 메일 전송
+	s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+	s.login(MAIL_SENDER, MAIL_PASSWORD)
+	send_email('changhyunlyoo@gmail.com', ip)
+	s.quit()
